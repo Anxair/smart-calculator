a03730beb319b8e5fef48aa934b96447b48dc261
Anastasiya <anxaira@gmail.com>
2018-09-28 13:48:42 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,56 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.expression = initialValue;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    add(number) {
+        this.expression += "+" + number;
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.expression += "-" + number;
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.expression += "*" + number;
+        return this;
+    }
+
+    devide(number) {
+        this.expression += "/" + number;
+        return this;
+    }
+
+    pow(number) {
+        this.expression += "^" + number;
+        return this;
+    }
+
+    solution(expression) {
+        let initPowArr = expression.match(/(([0-9]+\^)+[0-9])/g);
+        let transformPowArr = [];
+        if (initPowArr !== null) {
+            for (let i = 0; i < initPowArr.length; i++) {
+                let splitArr = initPowArr[i].split('^');
+                for (let j = splitArr.length - 2; j >= 0; j--) {
+                    splitArr[j] = ((Math.pow(parseInt(splitArr[j]), parseInt(splitArr[j + 1]))).toString());
+                }
+                transformPowArr.push(splitArr[0]);
+            }
+            for (let i = 0; i < transformPowArr.length; i++) {
+                expression = expression.replace(/(([0-9]+\^)+[0-9])/, transformPowArr[i]);
+            }
+        }
+        return eval(expression);
+    }
+
+    valueOf() {
+        return this.solution(this.expression);
+    }
+
 }
 
 module.exports = SmartCalculator;
